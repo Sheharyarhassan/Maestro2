449a85b2681b3a97ba0fd9ad3f1d36361c1f3366
Sheharyarhassan <shehryar@techtonex.com>
2024-10-16 21:23:35 +0500

    Audio Autoplay

--- src/pages/Instruments/index.js
+++ src/pages/Instruments/index.js
@@ -1,12 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef  } from "react";
 import Banner from "./Banner";
 import InstrumentModal from "./InstrumentModal";
 import Instruments from '../../assets/images/Images-Maestro/New Assets/Instruments - yellow.png'
+import audioFile from '../../assets/images/Images-Maestro/New Assets/INSTRUMENTS.mp3'
 import { Container } from "reactstrap";
 const Index = () => {
   document.title = "Instruments - Young Maestro";
   const [modalOpen, setModalOpen] = useState(false);
   const toggleModal = () => setModalOpen(!modalOpen);
+  const audioRef = useRef(null);
+
+  useEffect(() => {  
+    const handlePlay = async () => {  
+      try {  
+          audioRef.current.muted = false;
+      } catch (error) {  
+        console.error("Audio playback failed:", error);  
+      }  
+    };  
+
+    handlePlay();  
+  }, []);  
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -15,6 +29,7 @@ const Index = () => {
   return (
     <React.Fragment>
       <Banner />
+      <audio ref={audioRef} src={audioFile} muted autoPlay/>
       <div style={{ backgroundColor: "#21264c", height: "100%" }}>
         <Container>
           <img src={Instruments} alt="Instrument" className="mw-100 h-auto py-5" role="button" onClick={toggleModal}/>
